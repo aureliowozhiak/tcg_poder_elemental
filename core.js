ed486c3d7526ab9a6b088cd1add9416a7c2dec36
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2023-04-13 14:07:47 -0300

    Add image load

--- core.js
+++ core.js
@@ -1,104 +1,16 @@
 const cartas = {
-  "Boto-cor-de-bromélia": {
-  nome: "Boto-cor-de-bromélia",
-  ataque: 30,
-  defesa: 10,
-  },
-  "Boto-cor-de-rosa": {
-  nome: "Boto-cor-de-rosa",
-  ataque: 35,
-  defesa: 20,
-  },
-  "Boto-cor-de-margarida": {
-  nome: "Boto-cor-de-margarida",
-  ataque: 20,
-  defesa: 25,
-  },
-  "Boto-cor-de-orquídea": {
-  nome: "Boto-cor-de-orquídea",
-  ataque: 25,
-  defesa: 30,
-  },
-  "Elfo Aquático": {
-  nome: "Elfo Aquático",
-  ataque: 50,
-  defesa: 30,
-  },
   Iara: {
   nome: "Iara",
   ataque: 75,
   defesa: 55,
+  img:"iara.png"
   },
-  "Dragão Bebê": {
-  nome: "Dragão Bebê",
-  ataque: 20,
-  defesa: 20,
-  },
-  "Dragão Jovem": {
-  nome: "Dragão Jovem",
-  ataque: 50,
-  defesa: 30,
-  },
-  "Senhor Dragão": {
-  nome: "Senhor Dragão",
-  ataque: 60,
-  defesa: 85,
-  },
-  "Pônei Flamejante": {
-  nome: "Pônei Flamejante",
-  ataque: 50,
-  defesa: 50,
-  },
-  "Palhaço em Chamas": {
-  nome: "Palhaço em Chamas",
-  ataque: 35,
-  defesa: 40,
-  },
-  Boitatá: {
-  nome: "Boitatá",
-  ataque: 20,
-  defesa: 70,
-  },
-  "Dragão Elétrico": {
-  nome: "Dragão Elétrico",
-  ataque: 95,
-  defesa: 80,
-  },
-  "Alma Abençoada": {
-  nome: "Alma Abençoada",
-  ataque: 50,
-  defesa: 60,
-  },
-  "Cavaleiro Iluminado": {
-  nome: "Cavaleiro Iluminado",
-  ataque: 40,
-  defesa: 80,
-  },
-  "Centopeia do Trovão": {
-  nome: "Centopeia do Trovão",
-  ataque: 50,
-  defesa: 80,
-  },
-  "Elfo Elétrico": {
-  nome: "Elfo Elétrico",
+  "Elfo": {
+  nome: "Elfo",
   ataque: 45,
   defesa: 15,
+  img:"elfo.png"
   },
-  "Muralha Ambulante": {
-  nome: "Muralha Ambulante",
-  ataque: 0,
-  defesa: 100,
-  },
-  "Robô Defeituoso": {
-  nome: "Robô Defeituoso",
-  ataque: 20,
-  defesa: 10,
-  },
-  "Cavaleiro-Robô": {
-  nome: "Cavaleiro-Robô",
-  ataque: 70,
-  defesa: 20,
-  }
 };
 
 //Filtra as informações do monstro escolhido
@@ -154,6 +66,9 @@ document.querySelector(".home-select").addEventListener("change", (e) => {
   document.querySelector(".home-nomemonstro").innerHTML = monstroSelecionado.nome;
   document.querySelector(".home-ataque").innerHTML = "ataque: " + monstroSelecionado.ataque;
   document.querySelector(".home-defesa").innerHTML = "defesa: " + monstroSelecionado.defesa;
+  var img = document.querySelector("body > div > div > div.home-batalha > div.home-monstrojogador > img");
+  img.src = "http://poder-elemental.jvmsolutions.tech/cards_imgs/"+monstroSelecionado.img;
+
 });
 
 
@@ -164,6 +79,9 @@ function batalhar(){
   document.querySelector(".home-nomemonstro1").innerHTML = monstroAleatorio.nome;
   document.querySelector(".home-ataque1").innerHTML = "ataque: " + monstroAleatorio.ataque;
   document.querySelector(".home-defesa1").innerHTML = "defesa: " + monstroAleatorio.defesa;
+
+  var img = document.querySelector("body > div > div > div.home-batalha > div.home-monstroadversario > img");
+  img.src = "http://poder-elemental.jvmsolutions.tech/cards_imgs/"+monstroAleatorio.img;
 
 
 
