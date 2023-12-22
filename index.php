8af52ff41f9184ee8f0c12c022826f9e9a46b515
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2023-12-22 13:01:05 -0300

    new feat

--- index.php
+++ index.php
@@ -55,6 +55,32 @@
     
   
 <?php
+
+function getOpenAIResponse($apiKey, $userInput) {
+  $url = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
+  $headers = array(
+      'Content-Type: application/json',
+      'Authorization: Bearer ' . $apiKey
+  );
+  $data = array(
+      'prompt' => $userInput,
+      'max_tokens' => 1200
+  );
+
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+  $response = curl_exec($ch);
+  curl_close($ch);
+
+  $responseData = json_decode($response, true);
+  return $responseData['choices'][0]['text'];
+}
+
+getOpenAIResponse('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', 'Crie um monstro com nome, ataque e defesa no formato:');
+
 $cartas = [
     "monstro1" => [
         "nome" => "Dragão",
@@ -75,6 +101,56 @@ $cartas = [
         "nome" => "Mico leão dourado",
         "ataque" => 4,
         "defesa" => 4
+    ],
+    "monstro5" => [
+      "nome" => "Fênix",
+      "ataque" => 12,
+      "defesa" => 6
+    ],
+    "monstro6" => [
+      "nome" => "Kraken",
+      "ataque" => 9,
+      "defesa" => 11
+    ],
+    "monstro7" => [
+      "nome" => "Espectro",
+      "ataque" => 7,
+      "defesa" => 9
+    ],
+    "monstro8" => [
+      "nome" => "Grifo",
+      "ataque" => 11,
+      "defesa" => 7
+    ],
+    "monstro9" => [
+      "nome" => "Cerberus",
+      "ataque" => 10,
+      "defesa" => 10
+    ],
+    "monstro10" => [
+      "nome" => "Sereia",
+      "ataque" => 5,
+      "defesa" => 7
+    ],
+    "monstro11" => [
+      "nome" => "Gólem",
+      "ataque" => 8,
+      "defesa" => 12
+    ],
+    "monstro12" => [
+      "nome" => "Vampiro",
+      "ataque" => 9,
+      "defesa" => 5
+    ],
+    "monstro13" => [
+      "nome" => "Yeti",
+      "ataque" => 7,
+      "defesa" => 11
+    ],
+    "monstro14" => [
+      "nome" => "Harpia",
+      "ataque" => 6,
+      "defesa" => 8
     ]
 ];
 
